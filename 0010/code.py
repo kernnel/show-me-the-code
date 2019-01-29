12a98b23ccf91d0e3b29d047001db59ecbd78d58
佳  杨 <598049390@qq.com>
2019-01-29 16:16:08 +0800

    添加验证码

---
+++ 0010/code.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-  
+"""
+@author:kernnel 
+@file: code.py 
+@time: 2019/01/29 14:55
+@contact: 282458466@qq.com
+"""
+
+import random
+
+from PIL import Image, ImageDraw, ImageFont
+
+
+# 随机输出四个字母:
+def rndChar():
+    str = ''
+    for i in range(4):
+        str += chr(random.randint(65, 90))
+    return str
+
+
+# 随机颜色2:
+def rndColor2():
+    return (random.randint(32, 127), random.randint(32, 127), random.randint(32, 127))
+
+
+font = ImageFont.truetype('Arial.ttf', 66)
+# 打开一个jpg图像文件，注意是当前路径:
+width = 200
+height = 80
+im = Image.new('RGB', (width, height), rndColor2())
+# 获得图像尺寸:
+w, h = im.size
+print('Original image size: %sx%s' % (w, h))
+# 创建Draw对象:
+draw = ImageDraw.Draw(im)
+draw.text((0, 0), rndChar(), font=font, fill=rndColor2())
+# 缩放到50%:
+# im.thumbnail((w//2, h//2))
+# 把缩放后的图像用jpeg格式保存:
+im.save('code.jpg', 'jpeg')
